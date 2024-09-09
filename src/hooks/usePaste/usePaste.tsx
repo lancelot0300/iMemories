ee7a87dfdb687bb92d1442cb6a9158546a54135a
lancelot0300 <masza221@gmail.com>
2024-09-09 11:46:09 +0200

    update paste bug

--- src/hooks/usePaste/usePaste.tsx
+++ src/hooks/usePaste/usePaste.tsx
@@ -1,49 +1,59 @@
-import React from 'react'
-import useAxiosPrivate from '../useAxiosPrivate/useAxiosPrivate';
-import { useAppDispatch } from '../../state/store';
-import { setLastCommand } from '../../state/features/files/filesSlice';
-import { setPathAsync } from '../../state/features/path/pathSlice';
-import { Path, SelectedElements } from '../../types';
+import React from "react";
+import useAxiosPrivate from "../useAxiosPrivate/useAxiosPrivate";
+import { useAppDispatch } from "../../state/store";
+import { setLastCommand } from "../../state/features/files/filesSlice";
+import { setPathAsync } from "../../state/features/path/pathSlice";
+import { Path, SelectedElements } from "../../types";
 
 type Props = {
-    storageFiles: SelectedElements;
-    selectedFiles: SelectedElements;
-    actialPath: Path;
-    setIsOpened: React.Dispatch<React.SetStateAction<boolean>>;
+  storageFiles: SelectedElements;
+  selectedFiles: SelectedElements;
+  actialPath: Path;
+  setIsOpened: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-function usePaste( { selectedFiles,storageFiles, actialPath, setIsOpened} : Props) {
-    const axiosPrivate = useAxiosPrivate();
-    const dispatch = useAppDispatch();
+function usePaste({
+  selectedFiles,
+  storageFiles,
+  actialPath,
+  setIsOpened,
+}: Props) {
+  const axiosPrivate = useAxiosPrivate();
+  const dispatch = useAppDispatch();
 
-    const handlePasteClick = async () => {
-        const URL = `${process.env.REACT_APP_API_URL}/copy/foldersandfiles`;
-    
-        const filesids = storageFiles.map((file) => file.fileDetails?.id).filter((id) => id);
-        const foldersids = storageFiles.map((file) => file.folderDetails?.id).filter((id) => id);
-    
-          console.log(filesids, foldersids, actialPath.path);
-         await axiosPrivate.post(
-          URL,
-          {
-            filesids,
-            foldersids,
-            targetFolderId: actialPath.path,
-          },
-          {
-            withCredentials: true,
-          }
-        ).then(() => {
-          dispatch(setLastCommand({ files: selectedFiles, command: "paste" }));
-          dispatch(setPathAsync(actialPath.path));
-        })
-        .catch((error) => {
-          console.error("Error pasting files:", error);
-        })
-        setIsOpened(false);
-      };
+  const handlePasteClick = async () => {
+    const URL = `${process.env.REACT_APP_API_URL}/copy/foldersandfiles`;
 
-      return { handlePasteClick }
+    const filesids = storageFiles
+      .map((file) => file.fileDetails?.id)
+      .filter((id) => id);
+    const foldersids = storageFiles
+      .map((file) => file.folderDetails?.id)
+      .filter((id) => id);
+
+    const requset = axiosPrivate.post(
+      URL,
+      {
+        filesids,
+        foldersids,
+        targetFolderId: actialPath.path,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    dispatch(setLastCommand({ files: selectedFiles, command: "paste" }));
+    await requset
+      .then(() => {
+        dispatch(setPathAsync(actialPath.path));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setIsOpened(false);
+  };
+
+  return { handlePasteClick };
 }
 
-export default usePaste
+export default usePaste;
